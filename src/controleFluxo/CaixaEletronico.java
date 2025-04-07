69938fd42cafe05074b03026e1557f2e3d0cb3aa
Jimmie Haskell <haskell4228@gmail.com>
2025-04-06 22:15:27 -0300

    estudo do switch case

--- src/controleFluxo/CaixaEletronico.java
+++ src/controleFluxo/CaixaEletronico.java
@@ -44,7 +44,39 @@ public class CaixaEletronico {
 
         System.err.printf("Resultado: %s.\n", resultado);
     }
+
+    public static void sistemaMedida() {
+        String sigla = "G";
+
+        // if (sigla == "P") {
+        //     System.out.println("PEQUENO");
+        // } else if (sigla == "M") {
+        //     System.out.println("MEDIO");
+        // } else if (sigla == "G") {
+        //     System.out.println("GRANDE");
+        // } else {
+        //     System.out.println("INDEFINIDO");
+        // }
+
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MEDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+        }
+    }
+
     public static void main(String[] args) {
-        resultadoEscolar();
+        sistemaMedida();
     }
 }
