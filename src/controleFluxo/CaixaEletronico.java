90443322d8833e82f7acd5358f7c170d17f2f073
Jimmie Haskell <haskell4228@gmail.com>
2025-04-06 22:00:28 -0300

    controle de fluxo encadeado

--- src/controleFluxo/CaixaEletronico.java
+++ src/controleFluxo/CaixaEletronico.java
@@ -4,17 +4,20 @@ public class CaixaEletronico {
 
     public static void controleFluxoSimples() {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
         if (valorSolicitado < saldo) {
             saldo -= valorSolicitado;
+            System.out.printf("Novo saldo: R$ %.2f\n", saldo);
+        } else {
+            System.out.println("Saldo insuficiente");
         }
 
         System.out.printf("Saldo: R$ %.2f\n", saldo);
     }
 
     public static void controleFluxoComposto() {
-        int nota = 6;
+        int nota = 8;
 
         if (nota >= 7) {
             System.out.println("Aprovado");
@@ -23,7 +26,18 @@ public class CaixaEletronico {
         }
     }
 
+    public static void controleFluxoEncadeado() {
+        double nota = 6.9;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova de recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
     public static void main(String[] args) {
-        controleFluxoComposto();
+        controleFluxoEncadeado();
     }
 }
